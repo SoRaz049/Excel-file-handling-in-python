78d66fa2f08cf727388e53eaf244dea13a3c2000
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2022-05-03 18:08:16 +0545

    new

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import openpyxl
 
 excel_sheet= openpyxl.load_workbook ("D:\\Projects\\Python\\Excel file handling\\account_sheet.xlsx")
 
-excel_sheet.active.title
+work_sheet= excel_sheet.active
 
 sheet1= excel_sheet['Sheet1']
 

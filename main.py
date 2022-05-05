4ef1c869e62b208566b5d92e4144111fa8f259ff
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2022-05-05 17:54:34 +0545

    new commit

--- main.py
+++ main.py
@@ -1,11 +1,33 @@
 from openpyxl import Workbook, load_workbook
+from openpyxl.utils import get_column_letter
 
-excel_sheet= load_workbook ("D:\\Projects\\Python\\Excel file handling\\account_sheet.xlsx")
+acc_list=[]
 
-work_sheet= excel_sheet.active
+wb= load_workbook ("D:\\Projects\\Python\\Excel file handling\\account_sheet.xlsx")
+
+ws= wb.active
 
 #s1= excel_sheet['Sheet1']
 
-sender=input("Enter the account number of the sender: ")
-reciver=input("Enter the account number of the reciptant: ")
 
+
+#eciver=input("Enter the account number of the reciptant: ")
+
+for col in range (2,3):
+    for row in range (2,11):
+        char = get_column_letter( col )
+        
+        acc_no= ws [char + str(row)].value
+        acc_list.append(acc_no)
+        
+
+print(acc_list)
+        
+sender=int(input("Enter the account number of the sender: "))
+
+if sender in acc_list:
+    print ("valid")
+
+else:
+    print("Invalid")
+    

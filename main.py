270af0e5a753ff99f00735cf2edaacd96e8ef27e
Swaraj Barnwal <swarajbarnwal987@gmailcom>
2022-05-07 10:50:37 +0545

    yeti ta sakiyo yr

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from tkinter.tix import Y_REGION
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
 acc_list=[]
+pp_bal=[]
 
 wb= load_workbook ("D:\\Projects\\Python\\Excel file handling\\account_sheet.xlsx")
 
@@ -21,8 +23,24 @@ print(acc_list)
 sender=int (input("Enter the account number of the sender: "))
 receiver=int (input("Enter the account number of the reciptant: "))
 
-if sender in acc_list:
+sen_index=(acc_list.index(str (sender)))
+rec_index=(acc_list.index(str (receiver)))
 
-    if receiver in acc_list:
-        
-        
+
+# for col in range (3,4):
+#     for row in range (2,11):
+#         char=get_column_letter(col)
+#         p_balance= ws [char + str(row)].value
+#         pp_bal.append(p_balance)
+
+send_amount = int (input("Enter how much money you want to tansfer? "))
+sender_previous = int(ws['C'+ str(sen_index + 2)].value) 
+receiver_previous = int(ws['C'+ str(rec_index + 2)].value) 
+
+ws['C'+ str(sen_index + 2)].value = sender_previous - send_amount
+ws['C'+ str(rec_index + 2)].value = receiver_previous + send_amount
+print("vayo yr")
+print(ws['C'+ str(rec_index + 2)].value)
+
+wb.save("D:\\Projects\\Python\\Excel file handling\\account_sheet.xlsx")
+
